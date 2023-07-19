1df1fe80ae921f81307740269f8a66c065d0dc6a
chanchan321 <116455671+chanchan321@users.noreply.github.com>
2023-07-19 14:26:29 +0800

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -6,26 +6,19 @@ const whitelist = [
 const config = {
     headers: {
         "Access-Control-Allow-Origin": "https://newfront19.onrender.com",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-        "Access-Control-Request-Headers": "*",
-        "Access-Control-Allow-Headers": "Accept"
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
         }
   };
 
 const corsOptions = {
-    // origin: (origin, callback) => {
-    //     if (whitelist.indexOf(origin) !== -1) {
-    //         callback(null, true)
-    //     } else {
-    //         callback(new Error('Not allowed by CORSs'));
-    //     }
-    // },
-    headers: {
-        "Access-Control-Allow-Origin": "https://newfront19.onrender.com",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-        "Access-Control-Request-Headers": "*",
-        "Access-Control-Allow-Headers": "Accept"
-        },
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORSs'));
+        }
+    },
+    config,
     credentials: true,
     optionsSuccessStatus: 200
 }
