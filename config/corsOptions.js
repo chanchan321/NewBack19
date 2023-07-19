b375f760cdd9410dcdeee65279ef4cd240807ce7
chanchan321 <116455671+chanchan321@users.noreply.github.com>
2023-07-19 16:46:49 +0800

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -5,7 +5,7 @@ const whitelist = [
 
 const config = {
     headers: {
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": 'https://newfront19.onrender.com','https://newfront19.onrender.com/'
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
         }
   };
