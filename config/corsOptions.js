e7d44100a713890878d78a4adc27a82e14b24871
chanchan321 <116455671+chanchan321@users.noreply.github.com>
2023-07-19 17:09:35 +0800

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -7,7 +7,7 @@ const config = {
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Headers": "*"
         }
   };
 
