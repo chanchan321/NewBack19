7fb22b634b481162e89cd6bbf4c9c191c39326b4
chanchan321 <116455671+chanchan321@users.noreply.github.com>
2023-07-19 16:09:56 +0800

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,22 +3,22 @@ const whitelist = [
 
 ];
 
-// const config = {
-//     headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-//         }
-//   };
+const config = {
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+        }
+  };
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORSs'));
-        }
-    },
-    // config,
+    // origin: (origin, callback) => {
+    //     if (whitelist.indexOf(origin) !== -1) {
+    //         callback(null, true)
+    //     } else {
+    //         callback(new Error('Not allowed by CORSs'));
+    //     }
+    // },
+    config,
     credentials: true,
     optionsSuccessStatus: 200
 }
